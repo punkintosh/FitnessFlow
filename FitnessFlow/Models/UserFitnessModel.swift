f25e6eb65a4da393fc2ec85932dd64ab4f3caafc
Punkintosh <128147625+punkintosh@users.noreply.github.com>
2023-05-20 13:22:49 +0530

    Update user's health and fitness models

--- FitnessFlow/Models/UserFitnessModel.swift
+++ FitnessFlow/Models/UserFitnessModel.swift
@@ -6,3 +6,15 @@
 //
 
 import Foundation
+
+struct UserFitnessModel {
+    let fitnessGoal: String
+    let fitnessLevel: String
+    let weeklyGoal: Int
+    
+    init(fitnessGoal: String, fitnessLevel: String, weeklyGoal: Int) {
+        self.fitnessGoal = fitnessGoal
+        self.fitnessLevel = fitnessLevel
+        self.weeklyGoal = weeklyGoal
+    }
+}
--- FitnessFlow/Models/UserHealthModel.swift
+++ FitnessFlow/Models/UserHealthModel.swift
@@ -10,24 +10,16 @@ import Foundation
 struct UserHealthModel {
     let height: Double
     let weight: Double
-    let bmi: Double
     let age: Int
     let gender: String
-    let fitnessGoal: String
-    let fitnessLevel: String
     let healthConditions: Array<String>
-    let exercisePreferences: Array<String>
     
-    init(height: Double, weight: Double, bmi: Double, age: Int, gender: String, fitnessGoal: String, fitnessLevel: String, healthConditions: Array<String>, exercisePreferences: Array<String>) {
+    init(height: Double, weight: Double, bmi: Double, age: Int, gender: String, healthConditions: Array<String>) {
         self.height = height
         self.weight = weight
-        self.bmi = bmi
         self.age = age
         self.gender = gender
-        self.fitnessGoal = fitnessGoal
-        self.fitnessLevel = fitnessLevel
         self.healthConditions = healthConditions
-        self.exercisePreferences = exercisePreferences
     }
 }
 
